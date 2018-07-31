5ddc5e893cf29c42abd768c995a780f0ffff70ce
Kamel Abdelouahab <kamel_eddine.abdelouahab@uca.fr>
2018-07-31 18:10:40 +0200

    Tested Caffe implementation of Lenet 0.9883 on MNIST, 0.7087 on USPS

--- script/report_tpr.sh
+++ script/report_tpr.sh
@@ -1,4 +1,2 @@
-caffe test -solver caffe/networks/caffe-lenet_train.prototxt \
-           -weights caffe/networks/caffe-lenet.caffemodel \
-           - gpu 0 \
-           - iterations 100 \
+caffe test -model caffe/networks/caffe-lenet_train.prototxt -weights caffe/networks/caffe-lenet.caffemodel -iterations 500 -gpu 0
+caffe test -model caffe/networks/caffe-lenet_test.prototxt -weights caffe/networks/caffe-lenet.caffemodel -iterations 500 -gpu 0
