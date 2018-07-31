a6bf47d81fc5ff52185b508b549d54907f932875
Kamel Abdelouahab <kamel_eddine.abdelouahab@uca.fr>
2018-07-31 18:58:16 +0200

    Added results of Lecun original Lenet5, 0.8996 on MNIST, 0.8938 on USPS

--- script/report_tpr.sh
+++ script/report_tpr.sh
@@ -1,2 +1,4 @@
-caffe test -model caffe/networks/caffe-lenet_train.prototxt -weights caffe/networks/caffe-lenet.caffemodel -iterations 500 -gpu 0
-caffe test -model caffe/networks/caffe-lenet_test.prototxt -weights caffe/networks/caffe-lenet.caffemodel -iterations 500 -gpu 0
+# caffe test -model caffe/networks/caffe-lenet_train.prototxt -weights caffe/networks/caffe-lenet.caffemodel -iterations 500 -gpu 0
+# caffe test -model caffe/networks/caffe-lenet_test.prototxt -weights caffe/networks/caffe-lenet.caffemodel -iterations 500 -gpu 0
+caffe test -model caffe/networks/lecun-lenet_train.prototxt -weights caffe/networks/lecun-lenet.caffemodel -iterations 500 -gpu 0
+caffe test -model caffe/networks/lecun-lenet_test.prototxt -weights caffe/networks/lecun-lenet.caffemodel -iterations 500 -gpu 0
--- script/train.sh
+++ script/train.sh
@@ -1,1 +1,2 @@
-caffe train --solver=caffe/networks/caffe-lenet_solver.prototxt
+# caffe train --solver=caffe/networks/caffe-lenet_solver.prototxt
+caffe train --solver=caffe/networks/lecun-lenet_solver.prototxt
