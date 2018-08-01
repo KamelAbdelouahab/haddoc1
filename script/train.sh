e864c73e94a52a681369de938dc97fe8d5157489
Kamel Abdelouahab <kamel_eddine.abdelouahab@uca.fr>
2018-08-01 09:37:04 +0200

    Trained dream lenet, 0.9849 on MNIST, 0.7608 on USPS

--- script/train.sh
+++ script/train.sh
@@ -1,2 +1,3 @@
 # caffe train --solver=caffe/networks/caffe-lenet_solver.prototxt
-caffe train --solver=caffe/networks/lecun-lenet_solver.prototxt
+# caffe train --solver=caffe/networks/lecun-lenet_solver.prototxt
+caffe train --solver=caffe/networks/dream-lenet_solver.prototxt
