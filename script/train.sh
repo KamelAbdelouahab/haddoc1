da131d43d380c039be9386a0912482ac32127d3c
Kamel Abdelouahab <kamel_eddine.abdelouahab@uca.fr>
2018-08-08 10:54:09 +0200

    Added caffe-lenet with 3x3 kernels

--- script/train.sh
+++ script/train.sh
@@ -1,3 +1,4 @@
 # caffe train --solver=caffe/networks/caffe-lenet_solver.prototxt
 # caffe train --solver=caffe/networks/lecun-lenet_solver.prototxt
-caffe train --solver=caffe/networks/dream-lenet_solver.prototxt
+# caffe train --solver=caffe/networks/dream-lenet_solver.prototxt
+caffe train --solver=caffe/networks/caffe3-lenet_solver.prototxt
