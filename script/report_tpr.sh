09b582b75dc2c06c74f2fe95190229683090b736
Kamel Abdelouahab <kamel_eddine.abdelouahab@uca.fr>
2018-09-14 12:35:39 +0200

    Updated I4 implementation

--- script/report_tpr.sh
+++ script/report_tpr.sh
@@ -2,7 +2,7 @@
 # caffe test -model caffe/networks/caffe-lenet_test.prototxt -weights caffe/networks/caffe-lenet.caffemodel -iterations 500 -gpu 0
 # caffe test -model caffe/networks/lecun-lenet_train.prototxt -weights caffe/networks/lecun-lenet.caffemodel -iterations 500 -gpu 0
 # caffe test -model caffe/networks/lecun-lenet_test.prototxt -weights caffe/networks/lecun-lenet.caffemodel -iterations 500 -gpu 0
-# caffe test -model caffe/networks/dream-lenet_train.prototxt -weights caffe/networks/dream-lenet.caffemodel -iterations 500 -gpu 0
-# caffe test -model caffe/networks/dream-lenet_test.prototxt -weights caffe/networks/dream-lenet.caffemodel -iterations 500 -gpu 0
-caffe test -model caffe/networks/caffe3-lenet_train.prototxt -weights caffe/networks/caffe3-lenet.caffemodel -iterations 500 -gpu 0
-caffe test -model caffe/networks/caffe3-lenet_test.prototxt  -weights caffe/networks/caffe3-lenet.caffemodel -iterations 500 -gpu 0
+caffe test -model caffe/networks/dream-lenet_train.prototxt -weights caffe/networks/dream-lenet.caffemodel -iterations 500 -gpu 0
+caffe test -model caffe/networks/dream-lenet_test.prototxt -weights caffe/networks/dream-lenet.caffemodel -iterations 500 -gpu 0
+# caffe test -model caffe/networks/caffe3-lenet_train.prototxt -weights caffe/networks/caffe3-lenet.caffemodel -iterations 500 -gpu 0
+# caffe test -model caffe/networks/caffe3-lenet_test.prototxt  -weights caffe/networks/caffe3-lenet.caffemodel -iterations 500 -gpu 0
--- script/train.sh
+++ script/train.sh
@@ -1,4 +1,5 @@
 # caffe train --solver=caffe/networks/caffe-lenet_solver.prototxt
 # caffe train --solver=caffe/networks/lecun-lenet_solver.prototxt
-# caffe train --solver=caffe/networks/dream-lenet_solver.prototxt
-caffe train --solver=caffe/networks/caffe3-lenet_solver.prototxt
+caffe train --solver=caffe/networks/dream-lenet_solver.prototxt
+mv caffe/networks/dream-lenet_iter_10000.caffemodel caffe/networks/dream-lenet.caffemodel
+# caffe train --solver=caffe/networks/caffe3-lenet_solver.prototxt
